3a597e2f64ad113170a073e316dd025555d7193c
xule <zheng.fang@richemont.com>
2021-07-10 22:33:37 +0800

    add lombok config

--- src/main/java/com/thoughtworks/springboothello/domain/Entity/User.java
+++ src/main/java/com/thoughtworks/springboothello/domain/Entity/User.java
@@ -1,28 +1,16 @@
 package com.thoughtworks.springboothello.domain.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
+@Data
+@ToString
+@AllArgsConstructor
+@Slf4j
 public class User {
     private String name;
     private int age;
 
-    public User(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
